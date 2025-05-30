57ef8141251c0e2c41570a6a6ada86fa034a1cd5
Ajay Rajput <ajayrajthakur111@gmail.com>
2025-05-31 02:16:24 +0530

    route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,15 +1,20 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import { PublicRoute } from "./Public.route";
 import { DashboardRoute } from "./Dashboard.route";
+import getCookie from "@/utils/getCookie";
+import LoginPage from "@/views/Auth/Login";
 
 export const IndexRoutes = () => {
+  const token = getCookie("token");
   return (
     <>
       <Routes>
+        <Route
+          path="/"
+          element={token ? <Navigate to="/dashboard" replace /> : <LoginPage />}
+        />{" "}
         <Route path="/auth/*" element={<PublicRoute />} />
-
         <Route path="/dashboard/*" element={<DashboardRoute />} />
-
       </Routes>
     </>
   );
